d6d00feaffbe70ac89d4fc9acea688d04c9e52b7
undercrosshd <hd907@naver.com>
2025-04-04 17:33:58 +0900

    feat : 상품 상세페이지 사진, 상품 가격 UI

--- lib/food_detail_page.dart
+++ lib/food_detail_page.dart
@@ -9,6 +9,30 @@ class FoodDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(food['name']!)),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 350,
+            child: Image.asset(food['image']!,
+            fit: BoxFit.cover)),
+            SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text("상품 가격 : ${food['price']!}원",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold
+              )
+              ),
+              SizedBox(width: 8)
+            ]
+          ),
+              Text(food['description']!),
+              // 위젯 수량설정, 총가격, 구매하기 버튼, 장바구니 담기 버튼
+          ],
+      ),
 
     );
   }
