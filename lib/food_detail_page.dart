f25bd24e0cded5361eea0a5e110c377b7682c50a
undercrosshd <hd907@naver.com>
2025-04-04 17:50:12 +0900

    feat : 상품 설명 UI

--- lib/food_detail_page.dart
+++ lib/food_detail_page.dart
@@ -29,11 +29,26 @@ class FoodDetailPage extends StatelessWidget {
               SizedBox(width: 8)
             ]
           ),
-              Text(food['description']!),
+          SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 3),
+                width: 400,
+                height: 250,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1.0,
+                    style: BorderStyle.solid
+                  )
+                ),
+                child: Text(food['description']!,
+                style: TextStyle(
+                  fontSize: 18,
+                ))
+              ),
+              SizedBox(height: 20)
               // 위젯 수량설정, 총가격, 구매하기 버튼, 장바구니 담기 버튼
           ],
       ),
-
     );
   }
 }
